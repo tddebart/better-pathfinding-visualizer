29a65418222c64f2a799ba8a63bc630e038e124e
tddebart <tddebart@gmail.com>
2021-10-18 23:47:02 +0200

    github logo link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Visualizer from "./Visualizer/visualizer";
+import gitLogo from "./GitHub-Mark-64px.png"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <a href="https://github.com/tddebart/better-pathfinding-visualizer" target="_blank">
+            <img className={"github"} src={gitLogo} alt={"github"} />
+          </a>
           <Visualizer/>
       </header>
     </div>
